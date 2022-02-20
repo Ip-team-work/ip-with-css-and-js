5f26707ab3bde0a1ce8c64c80ca8822d1e89d96c
bisrat mekonnen <bmekonnen422@gmail.com>
2022-02-20 16:20:15 +0300

    edited homepage on the newidea folder

--- nav/nav-js.js
+++ nav/nav-js.js
@@ -1,39 +1,96 @@
-const navSlide=()=>{
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks= document.querySelectorAll('.nav-links li');
+$(document).ready(function(){
 
-    burger.addEventListener('click',()=>{
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-
-        //animation links
-        navLinks.forEach((link,index)=>{
-            if(link.style.animation){
-                link.style.animation='';
-            }
-            else{ 
-                link.style.animation= `navLinkFade .5s ease forwards ${index/7 + 0 }s`;
-            }
-        });
-    });
-}
-navSlide();
-
-/* nav bar scrolling  */
-
-var navbar= document.getElementById("navbar");
-var menu = document.getElementById("menu");
-window.onscroll=function(){
-    console.log("widnow");
-    if(window.pageYOffset<=menu.offsetTop){
-        navbar.classList.add("navadd");
-        console.log('ifhere');
+  $(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $("#header").addClass("active-header");
+        $(".menu-link").addClass("active-menu-links");
+        $("#header-img").attr("src", "https://i.ibb.co/0jM3ZvH/reverse-black.png");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $("#header").removeClass("active-header");
+       $(".menu-link").removeClass("active-menu-links");
+       $("#header-img").attr("src", "https://i.ibb.co/LxGJ5gF/reverse-white.png");
     }
-    else{
-        
-        navbar.classList.remove("navadd");
-        console.log("elseifhere");
-    }
-}
-/* nav bar scrolling end */
+});
+
+  // Responsive menu
+  $("#responsive-nav-bar").hide();
+
+  $("#image-button-black").click(function(){
+    $("#responsive-nav-bar").slideToggle();
+  });
+
+  $("#responsive-home-item , #responsive-features-item , #responsive-products-item , #responsive-media-item , #responsive-pricing-item").click(function(){
+    $("#responsive-nav-bar").slideToggle(200);
+  })
+
+
+
+
+
+  // Default sections visibility
+  $('.product-content').hide();
+  $('#product-container-one').show();
+
+
+  // Button variables
+  var firstButton = $('#first-product-button');
+  var secondButton = $('#second-product-button');
+  var thirdButton = $('#third-product-button');
+  var fourthButton = $('#fourth-product-button');
+  var buttons = $('.product-button');
+
+
+
+  // Product variables
+  var firstProduct = $('#product-container-one');
+  var secondProduct = $('#product-container-two');
+  var thirdProduct = $('#product-container-three');
+  var fourthProduct = $('#product-container-four');
+
+
+  // Visibility variables
+  var firstVisible = firstProduct.is(':visible');
+  var secondVisible = secondProduct.is(':visible');
+  var thirdVisible = thirdProduct.is(':visible');
+  var fourthVisible = fourthProduct.is(':visible');
+
+
+  // Visible and Hidden Sections .click function
+  $(firstButton).click(function(){
+    $('.product-content').hide();
+    $(firstProduct).fadeIn(600);
+    buttons.removeClass('product-button-active');
+    firstButton.addClass('product-button-active');
+  });
+
+  $(secondButton).click(function(){
+    $('.product-content').hide();
+    $(secondProduct).fadeIn(600);
+    $(secondProduct).css('visibility', 'visible');
+    buttons.removeClass('product-button-active');
+    secondButton.addClass('product-button-active');
+  });
+
+  $(thirdButton).click(function(){
+    $('.product-content').hide();
+    $(thirdProduct).fadeIn(600);
+    buttons.removeClass('product-button-active');
+    thirdButton.addClass('product-button-active');
+  });
+
+  $(fourthButton).click(function(){
+    $('.product-content').hide();
+    $(fourthProduct).fadeIn(600);
+    buttons.removeClass('product-button-active');
+    fourthButton.addClass('product-button-active');
+  });
+
+
+
+  // First Button Styling hidden/visible
+  if (firstVisible === true) {
+    firstButton.addClass('product-button-active');
+  }
+
+});
