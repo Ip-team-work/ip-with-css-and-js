24f1f4673bcb8c194185caf04c3e9a67d0f40ae5
natnaelLake <bewuketulake1212@gmail.com>
2022-06-07 14:04:56 +0300

    done

---
+++ FULL STACK TOUR/Admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© 2022G.C AASTU |  <a href="#">Abugida Tour and Travel</a> </p>
+</div>	
---
+++ FULL STACK TOUR/Admin/includes/header.php
@@ -0,0 +1,9 @@
+<link href="css/style1.css" rel='stylesheet' type='text/css' />
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+					<div class="logo-w3-agile">
+								<h1><a href="dashboard.php" >Tourism Management System</a></h1>
+							</div>
+				
+						
+						
+	
